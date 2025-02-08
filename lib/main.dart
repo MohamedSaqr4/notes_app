db5068d167cb8172f5632a8e20ba3c8094d6dd6f
Mohamed Saqr <mah1346478hany@gmail.com>
2025-02-08 23:33:16 +0200

    trigger add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/Models/NoteModel.dart';
+import 'package:note_app/Simple%20Block%20Observe.dart';
 import 'package:note_app/constants.dart';
 import 'package:note_app/cubits/Add%20Note%20Cubit/add_note_cubit.dart';
 import 'package:note_app/views/NotesView.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlockObserve();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
@@ -20,7 +22,9 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-      BlocProvider(create: (context) => AddNoteCubit(),)  
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        )
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
